041d07f7c68a669d116a9a1f4c6b1337908f2f7d
L F <445052171@std.psau.edu.sa>
2026-03-30 15:54:05 +0300

    Feature 1: added priority  to process class with a get method and changed the print statemnt in class SchedulerSimulation

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -30,12 +30,14 @@ class Process implements Runnable {
     private int timeQuantum; // Time slice (time quantum) allowed per CPU access (in milliseconds)
     private int remainingTime; // Time left for the process to finish its execution
 
+    private int priority;// added here field
     // Constructor to initialize the process with name, burst time, and time quantum
-    public Process(String name, int burstTime, int timeQuantum) {
+    public Process(String name, int burstTime, int timeQuantum ) {
         this.name = name;
         this.burstTime = burstTime;
         this.timeQuantum = timeQuantum;
         this.remainingTime = burstTime; // Initially, remaining time is equal to the burst time
+        priority=(int)(Math.random()*5)+1;//used math random to genrate random numbers 
     }
 
     // This method will be called when the thread for this process is started
@@ -141,6 +143,9 @@ class Process implements Runnable {
     public boolean isFinished() {
         return remainingTime <= 0;
     }
+    public int getPriority() {// get method for priority to retrive data for SchedulerSimulation
+    return priority;
+}
 }
 
 public class SchedulerSimulation {
@@ -292,7 +297,7 @@ public class SchedulerSimulation {
         
         // Print a message indicating the process has entered the ready queue
         System.out.println(Colors.BLUE + "  ➕ " + Colors.BOLD + Colors.CYAN + process.getName() + 
-                          Colors.RESET + Colors.BLUE + " added to ready queue" + Colors.RESET + 
+                          Colors.RESET + Colors.BLUE + " (Priority: " +process.getPriority()+") enters the ready queue"+ process.getPriority() + ")" + Colors.RESET + //used get method to retrive priority data and print it in the message
                           " │ Burst time: " + Colors.YELLOW + process.getBurstTime() + "ms" + 
                           Colors.RESET);
     }
